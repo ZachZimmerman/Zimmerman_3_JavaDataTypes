64034c5aecc70f93747584edf35445de3662d22b
Zach Zimmerman <zzachary54@gmail.com>
2015-09-30 06:04:40 -0500

    Primes

---
+++ src/zimmerman_3_data/types/Zimmerman_3_DataTypes.java
@@ -0,0 +1,65 @@
+
+package zimmerman_3_data.types;
+
+
+public class Zimmerman_3_DataTypes {
+    
+    public static void main(String[] args) {
+        //This is a comment
+        //we will use many comments to explain our code
+        //This program explores some of the primative DATA types
+        int i=0;
+        float a=0.01f;
+        double b=0.01;
+        short c=123;
+        char d1 = ';';
+        char d2 = 'a';
+        char d3 = 42;
+        char d4 = 145;       
+        String e="words";
+        boolean f = true;
+        boolean g = i % 2 == 0;
+        
+        /*
+        Here we will look at some mathmaical operators
+        
+        */
+        // + this adds two numbers and more
+        //- this subtracts two numbers and more
+        //* this multiplies two numbers and more
+        // / this divides two number and more
+        // % this is modulo. Shows the remainder 
+        //
+        
+        
+        System.out.println(i + " an integer");
+        System.out.println(a + " a float");
+        System.out.println(b + " a double");
+        System.out.println(c + " a short");
+        System.out.println(d1 + " a char");
+        System.out.println(d2 + " a char");
+        System.out.println(d3 + " a char");
+        System.out.println(d4 + " a char");
+        System.out.println(e + " a string");
+        System.out.println(f + " a boolean");
+        for (i= -123; i <123; i++) {
+        System.out.println((char) i);
+        //System.out.println(i % 0 == 0);
+        }
+        //a number value for char
+        //maps to a character
+        for (i= -123; i <123; i++) {
+       // i=0;
+       // a=0;
+//        System.out.println(i + a + " an integer plus a float"); 
+//        System.out.println(e + i + " an /" + "a String plus a interger" );
+//        System.out.println(a + " a float");
+//        System.out.println(b + " a double");
+//        System.out.println(c + " a short");
+        //System.out.println("Zach is a nerd, and Rhys is a Butt");
+            
+            
+        }
+    }
+    
+}
